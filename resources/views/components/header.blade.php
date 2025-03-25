0fdd4cd38c20fac0e2dcf168db57f2d85fa2930e
Jerson George <jersongeorgep@gmail.com>
2025-03-25 13:34:09 +0530

    updated

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -29,32 +29,25 @@
             <li><a class="dropdown-item" href="javascript:void(0);" data-language="en"><i class="fi fi-us fis rounded-circle me-1 fs-3"></i><span class="align-middle">English</span></a></li>
           </ul>
         </li>
-              <!--/ Language -->
-
-              <!-- Style Switcher -->
-              <li class="nav-item me-2 me-xl-0">
-                <a class="nav-link style-switcher-toggle hide-arrow" href="javascript:void(0);">
-                  <i class="ti ti-md"></i>
-                </a>
-              </li>
-              <!--/ Style Switcher -->
-
-              <!-- Quick links  -->
-              <li class="nav-item dropdown-shortcuts navbar-dropdown dropdown me-2 me-xl-0">
-                <a
-                  class="nav-link dropdown-toggle hide-arrow"
-                  href="javascript:void(0);"
-                  data-bs-toggle="dropdown"
-                  data-bs-auto-close="outside"
-                  aria-expanded="false">
-                  <i class="ti ti-layout-grid-add ti-md"></i>
-                </a>
-                <div class="dropdown-menu dropdown-menu-end py-0">
-                  <div class="dropdown-menu-header border-bottom">
-                    <div class="dropdown-header d-flex align-items-center py-3">
-                      <h5 class="text-body mb-0 me-auto">Shortcuts</h5>
-                      <a
-                        href="javascript:void(0)"
+        <!--/ Language -->
+        
+        <!-- Style Switcher -->
+         <li class="nav-item me-2 me-xl-0">
+          <a class="nav-link style-switcher-toggle hide-arrow" href="javascript:void(0);"><i class="ti ti-md"></i></a>
+        </li>
+        <!--/ Style Switcher -->
+        
+        <!-- Quick links  -->
+         <li class="nav-item dropdown-shortcuts navbar-dropdown dropdown me-2 me-xl-0">
+          <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false">
+            <i class="ti ti-layout-grid-add ti-md"></i>
+          </a>
+          
+          <div class="dropdown-menu dropdown-menu-end py-0">
+            <div class="dropdown-menu-header border-bottom">
+              <div class="dropdown-header d-flex align-items-center py-3">
+                <h5 class="text-body mb-0 me-auto">Shortcuts</h5>
+                <a href="javascript:void(0)"
                         class="dropdown-shortcuts-add text-body"
                         data-bs-toggle="tooltip"
                         data-bs-placement="top"
