c50917beb86c3651f121934f379f05fc0b5a0b88
Jerson George <jersongeorgep@gmail.com>
2025-04-15 17:38:34 +0530

    update

--- resources/views/components/forms/project-form.blade.php
+++ resources/views/components/forms/project-form.blade.php
@@ -1,22 +1,78 @@
-<form action="{{ route('recruitments.store-project') }}" method="post" id="formData">
+<form id="{{ $id ?? 'project-form' }}"  action="{{ $action }}" method="{{ strtolower($method) === 'get' ? 'get' : 'post' }}">
     @csrf
+    @if(strtolower($method) !== 'post')
+        @method($method)
+    @endif
+
     <div class="row">
-        <div class="col mb-3">
-            <label for="department_id" class="form-label">Department</label>
-            <select class="form-control" name="department_id" id="department_id" data-placeholder="Select Department" required>
-                <option value=""></option>
-                @if($departments->isNotEmpty())
-                    @foreach($departments as $result)
-                        <option value="{{ $result->id }}"> {{ $result->department }}</option>
-                    @endforeach
-                @endif
-            </select>
+        <div class="col-sm-12 mb-3">
+            <div class="form-group">
+                <label for="project_name">Project Name</label>
+                <input type="text" name="project_name" id="project_name" class="form-control" placeholder="Project Name"
+                    value="{{ old('project_name', $project->project_name ?? '') }}" />
+            </div>
         </div>
-        <div class="clearfix"></div>
-        <div class="col mb-3">
-            <label for="project_name" class="form-label">Project</label>
-            <input type="text" id="project_name" name="project_name" class="form-control" placeholder="Name">
+
+        <div class="col-sm-6 mb-3">
+            <div class="form-group">
+                <label for="project_add_person">Add By</label>
+                <select class="form-control select2" name="project_add_person" id="project_add_person" data-placeholder="Select Project">
+                    <option value=""></option>
+                    @foreach($users as $user)
+                        <option value="{{ $user->id }}" {{ old('project_add_person', $project->project_add_person ?? '') == $user->id ? 'selected' : '' }} >
+                            {{ $user->username }}
+                        </option>
+                    @endforeach
+                </select>
+            </div>
+        </div>
+
+        <div class="col-sm-6 mb-3">
+            <div class="form-group">
+                <label for="department_id">Department</label>
+                <select class="form-control select2" name="department_id" id="department_id" data-placeholder="Select Department">
+                    <option value=""></option>
+                    @foreach($departments as $department)
+                        <option value="{{ $department->id }}" {{ old('department_id', $project->department_id ?? '') == $department->id ? 'selected' : '' }} >
+                            {{ $department->department }}
+                        </option>
+                    @endforeach
+                </select>
+            </div>
+        </div>
+
+        <div class="col-sm-6 mb-3">
+            <div class="form-group">
+                <label for="start_date">Start Date</label>
+                <input type="date" name="start_date" id="start_date" class="form-control flatpickr-input" placeholder="Start Date" value="{{ old('start_date', $project->start_date ?? '') }}" />
+            </div>
+        </div>
+
+        <div class="col-sm-6 mb-3">
+            <div class="form-group">
+                <label for="end_date">End Date</label>
+                <input type="date" name="end_date" id="end_date" class="form-control flatpickr-input" placeholder="End Date" value="{{ old('end_date', $project->end_date ?? '') }}" />
+            </div>
+        </div>
+
+        <div class="col-sm-6 mb-3">
+            <div class="form-group">
+                <label for="total_hours">Total Hours</label>
+                <input type="text" name="total_hours" id="total_hours" class="form-control" placeholder="Total Hours"
+                    value="{{ old('total_hours', $project->total_hours ?? '') }}" />
+            </div>
+        </div>
+
+        <div class="col-sm-6 mb-3">
+            <div class="form-group">
+                <label for="total_day">Total days</label>
+                <input type="text" name="total_day" id="total_day" class="form-control" placeholder="Total Days"
+                    value="{{ old('total_day', $project->total_day ?? '') }}" />
+            </div>
+        </div>
+
+        <div class="col-sm-12 mb-3">
+            <button type="submit" class="btn btn-primary"><i class="fa fa-save"></i> &nbsp;&nbsp; Save</button>
         </div>
     </div>
 </form>
-                              
