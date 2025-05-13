2fa7c688150563263f6c95f8798ef55384478fe0
Jerson George <jersongeorgep@gmail.com>
2025-05-13 16:49:46 +0530

    updates user edit link

--- public/assets/js/app-user-list.js
+++ public/assets/js/app-user-list.js
@@ -216,12 +216,12 @@ $(function () {
         });
     });
 
-    document.addEventListener("click", function (e) {
-        if (e.target.closest(".edit-user")) {
-            const element = e.target.closest(".edit-user");
-            const userId = element.getAttribute("data-edit-user-id");
+    document.querySelectorAll(".edit-user").forEach((element) => {
+        element.addEventListener("click", function () {
+            let userId = this.getAttribute("data-edit-user-id"); // Corrected
+            // Redirect to the edit page
             window.location.href = `/users/${userId}/edit`;
-        }
+        });
     });
 }
 
