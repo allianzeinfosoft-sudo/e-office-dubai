f9e7b40ff70669f222d0fa4e38d98af5ff687407
Jerson George <jersongeorgep@gmail.com>
2025-05-13 17:00:09 +0530

    updates

--- public/assets/js/app-user-list.js
+++ public/assets/js/app-user-list.js
@@ -150,8 +150,8 @@ $(function () {
             const user_id =  full['id'];
             return (
               '<div class="d-flex align-items-center">' +
-              '<a href="javascript:;" class="text-body edit-user" data-edit-user-id="' + user_id + '"><i class="ti ti-edit ti-sm me-2"></i></a>' +
-              '<a href="javascript:;" class="text-body delete-user" data-user-id="' + user_id + '"><i class="ti ti-trash ti-sm mx-2"></i></a>' +
+              '<a href="javascript:void(0);" class="text-body edit-user" data-edit-user-id="' + user_id + '"><i class="ti ti-edit ti-sm me-2"></i></a>' +
+              '<a href="javascript:void(0);" class="text-body delete-user" data-user-id="' + user_id + '"><i class="ti ti-trash ti-sm mx-2"></i></a>' +
               '</div>'
             );
           }
