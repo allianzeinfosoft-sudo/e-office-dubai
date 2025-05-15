42e644855efc97f1050748d85a0e187bb2dd8f0b
Jerson George <jersongeorgep@gmail.com>
2025-05-15 17:00:44 +0530

    updates

--- app/View/Components/ProjectTaskForm.php
+++ app/View/Components/ProjectTaskForm.php
@@ -31,9 +31,8 @@ class ProjectTaskForm extends Component{
         $this->action       = $action;
         $this->method       = $method;
         $this->projectTask  = $projectTask;
-        //$reportingToIds     = Employee::whereNotNull('reporting_to') ->distinct()->pluck('reporting_to');
-        //$this->reportingTo  = Employee::whereIn('id', $reportingToIds)->get();
-        $this->reportingTo  = Employee::all();
+        $reportingToIds     = Employee::whereNotNull('reporting_to') ->distinct()->pluck('reporting_to');
+        $this->reportingTo  = Employee::whereIn('id', $reportingToIds)->get();
         $this->departments  = Department::all();
         $this->projects     = Project::all();
         $this->tasks        = Tasks::all();
