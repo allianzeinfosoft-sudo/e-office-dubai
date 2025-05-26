85cb8f87f99230d894e1aaefe4af04114c8855b5
Jerson George <jersongeorgep@gmail.com>
2025-05-26 11:42:40 +0530

    updates

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -240,7 +240,7 @@ public function getLeaveSummary(Request $request)
      $leaveBalance = $leave_allocated ? $leave_allocated->remaining_leaves : 0;
 
     return response()->json([
-        'leaveThisMonth' => ($leaveThisMonth - $offDays),
+        'leaveThisMonth' => ((int) $leaveThisMonth - (int) $offDays),
         'totalLeavesTaken' => $totalLeavesTaken,
         'pendingLeaves' => $pendingLeaves,
         'pastYearLeaves' => $pastYearLeaves,
