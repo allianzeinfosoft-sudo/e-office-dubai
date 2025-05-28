4c2c00b3f47cc736a60024f26607bd6b40cf1436
Jerson George <jersongeorgep@gmail.com>
2025-05-28 14:16:42 +0530

    updates

--- resources/views/attendance/work_report.blade.php
+++ resources/views/attendance/work_report.blade.php
@@ -305,7 +305,7 @@
 
         let html = '<tr data-id="${workReport.id}" >';
         html += `<td><strong>${workReport.project?.project_name || "-"}</strong></td>`;
-        html += `<td><strong>${workReport.project_task?.task_name || "-"}</strong></td>`;
+        html += `<td><strong>${workReport.tasks?.name || "-"}</strong></td>`;
         html += `<td><strong>${workReport.total_records || "-"}</strong></td>`;
         html += `<td><strong>${workReport.total_time || "00:00:00"}</strong></td>`;
         html += `<td><strong>${workReport.productivity_hour || "-"}</strong></td>`;
@@ -328,7 +328,7 @@
     let newRow = `
         <tr data-id="${workReport.id}">
             <td><strong>${workReport.project?.project_name || "-"}</strong></td>
-            <td><strong>${workReport.project_task?.task_name || "-"}</strong></td>
+            <td><strong>${workReport.tasks?.name || "-"}</strong></td>
             <td><strong>${workReport.total_records || "-"}</strong></td>
             <td><strong>${workReport.total_time || "00:00:00"}</strong></td>
             <td><strong>${workReport.productivity_hour || "-"}</strong></td>
