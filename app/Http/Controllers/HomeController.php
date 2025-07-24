4e2fe37b2391689faae238fdb5696f0ad34a67bd
Jerson George <jersongeorgep@gmail.com>
2025-07-24 13:32:47 +0530

    updates

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -58,7 +58,7 @@ class HomeController extends Controller
                 $minutes = floor(($seconds % 3600) / 60);
                 $seconds = $seconds % 60;
 
-                return sprintf('%02d:%02d:%02d', $hours, $minutes, $seconds);
+                return sprintf('%02d:%02d:%02d', $hours, $minutes);
             }
 
         $totalSeconds = 0;
