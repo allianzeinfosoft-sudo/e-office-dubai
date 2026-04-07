3bc655fd350f281e2e7081013f5513618674611a
Jerson George <jersongeorgep@gmail.com>
2026-04-07 11:33:08 +0530

    updates

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -27,12 +27,12 @@
       <!-- / Digital Clock -->
       
       <!-- Language -->
-      <li class="nav-item dropdown-language dropdown me-2 me-xl-0">
+      {{-- <li class="nav-item dropdown-language dropdown me-2 me-xl-0">
         <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown"><i class="fi fi-us fis rounded-circle me-1 fs-3"></i></a>
         <ul class="dropdown-menu dropdown-menu-end">
           <li><a class="dropdown-item" href="javascript:void(0);" data-language="en"><i class="fi fi-us fis rounded-circle me-1 fs-3"></i><span class="align-middle">English</span></a></li>
         </ul>
-      </li>
+      </li> --}}
       <!--/ Language -->
       
       <!-- Style Switcher -->
